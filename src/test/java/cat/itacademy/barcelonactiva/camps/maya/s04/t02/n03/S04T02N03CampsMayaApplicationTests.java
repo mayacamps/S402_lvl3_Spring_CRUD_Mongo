e9d57f0f0d4ec6b1f4c2e1d271a1cf11235e7efe
Maya <maya.camps@estudiantat.upc.edu>
2024-01-15 13:41:03 +0100

    first commit

---
+++ src/test/java/cat/itacademy/barcelonactiva/camps/maya/s04/t02/n03/S04T02N03CampsMayaApplicationTests.java
@@ -0,0 +1,13 @@
+package cat.itacademy.barcelonactiva.camps.maya.s04.t02.n03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S04T02N03CampsMayaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
